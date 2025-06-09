afe101b81e9d063378222b55708ba4d926790317
MiladTheGreat <persianboyland@gmail.com>
2025-06-09 11:50:42 +0330

    added git ignore , updated config file, added train loop to main , added updated models, added requirements file

--- models/model.py
+++ models/model.py
@@ -1,27 +1,48 @@
 import torch.nn as nn
+from torchvision import models 
 
-class SimpleMLP(nn.Module):
-    def __init__(self,input_size,num_classes):
-        super().__init__()
-        self.layer1 = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(input_size,128),
-            nn.ReLU(),
-            nn.Linear(128,64),
-            nn.ReLU(),
-            nn.Linear(64,num_classes)
-        )
-
-    def forward(self,x):
-        x = self.layer1(x)
-        return x
     
-def get_model(model_name,input_size,num_classes):
+def get_model(model_name,num_classes,params=None):
     model_name = model_name.lower()
-    if model_name == 'mlp':
-        return SimpleMLP(input_size,num_classes)
-    else:
-        raise ValueError(f"Model:{model_name} is not defined")
+    params = params or {}
+
+    if model_name in ['swin_t','swin_tiny']:
+        net = models.swin_t(weights="IMAGENET1K_V1" if params.get('pretrained',True) else None)
+        net.head = nn.Linear(net.head.in_features,num_classes)
+        if params.get('freeze',False):
+            for name,param in net.named_parameters():
+                if 'head' not in name:
+                    param.requires_grad = False
+        return net
     
 
+    elif model_name.startswith('efficientnet_v2_'):
+        net = getattr(models,model_name)(weights= "IMAGENET1K_V1" if params.get('pretrained',True) else None)
+        net.classifier[1] = nn.Linear(net.net.classifier[1].in_features,num_classes)
+        if params.get('freeze',False):
+            for name,param in net.named_parameters():
+                if 'classifier.1' not in name:
+                    param.requires_grad = False
+
+        return net
     
+    elif model_name.startswith('efficientnet_'):
+        net = getattr(models, model_name)(weights="IMAGENET1K_V1" if params.get('pretrained', True) else None)
+        net.classifier[1] = nn.Linear(net.classifier[1].in_features, num_classes)
+        if params.get('freeze', False):
+            for name, param in net.named_parameters():
+                if "classifier.1" not in name:
+                    param.requires_grad = False
+        return net
+    
+    elif model_name in ['vit_b_16', 'vit_b_32', 'vit_l_16', 'vit_l_32']:
+        net = getattr(models, model_name)(weights="IMAGENET1K_V1" if params.get('pretrained', True) else None)
+        net.heads.head = nn.Linear(net.heads.head.in_features, num_classes)
+        if params.get('freeze', False):
+            for name, param in net.named_parameters():
+                if "heads.head" not in name:
+                    param.requires_grad = False
+        return net
+    
+    else:
+        raise ValueError(f"Model '{model_name}' is not implemented.")   
